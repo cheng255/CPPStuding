012716db6c30dd8c0728b0a0af65ca89e4ef4637
cheng255 <18229094255@163.com>
2020-02-23 19:40:02 +0800

    提交代码

--- Frist/swap.cpp
+++
@@ -1,9 +0,0 @@
-//º¯ÊıµÄÄÚÈİ
-void swap(int *pa, int *pb)
-{
-
-	int temp = *pa;
-	*pa = *pb;
-	*pb = temp;
-
-}
--- Frist/swap.h
+++
@@ -1,5 +0,0 @@
-
-#include <iostream>
-using namespace std;
-//函数的定义
-void swap(int *pa, int *pb);
