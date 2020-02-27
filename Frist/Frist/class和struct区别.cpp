a57cbaca6f87c0bf51ad39bfefa0465dddadee3a
cheng255 <18229094255@163.com>
2020-02-27 19:15:52 +0800

    提交代码

---
+++ Frist/Frist/class和struct区别.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//using namespace std;
+//
+//class C1
+//{
+//	int m_A; //默认权限是  private
+//
+//};
+//
+//
+//struct C2
+//{
+//	int m_A; //默认权限是  public
+//};
+//
+//int main(){
+//
+//	//struct 和 class区别
+//	//struct 默认权限是 public
+//	//class  默认权限是 private
+//
+//
+//
+//	C1 c1;
+//	//c1.m_A = 100;不可访问
+//	C2 c2;
+//	c2.m_A = 100;//可以访问
+//
+//
+//	system("pause");
+//	return 0;
+//}
