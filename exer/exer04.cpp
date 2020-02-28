1b7fd33d36a6a6a3fca48e19dcb354281e98f874
cheng255 <18229094255@163.com>
2020-02-28 23:16:33 +0800

    提交代码

---
+++ exer/exer04.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include <string>
+//输入一个字符串，输出其长度。
+int test01(string s)
+{
+	return s.length();
+
+}
+
+
+
+int main(){
+	//test01测试
+	//string s = "aasdas";
+	//cout << s.length() << endl;
+	//cout << test01(s) << endl;
+
+
+
+	system("pause");
+	return 0;
+}
+
